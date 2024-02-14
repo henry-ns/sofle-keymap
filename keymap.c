b9170eae0af0dd1a72b74f1cd8a25a3d21338ea1
Henrique Miranda <enrque.ns@gmail.com>
2024-02-14 12:12:22 -0300

    fix gamer layout

--- keymap.c
+++ keymap.c
@@ -59,13 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */ 
-  
 [_GAMER] = LAYOUT(
-  KC_GRV,  KC_1, KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,       KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
-  KC_ESC,  KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-  KC_TAB,  KC_A, KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,       XXXXXXX,     XXXXXXX, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_SPC,      KC_ENT,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+  KC_ESC,  KC_1, KC_2,    KC_3,    KC_4,    KC_5,                                 KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
+  KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+  KC_CAPS, KC_A, KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,   XXXXXXX,          XXXXXXX,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                 XXXXXXX, KC_LALT, KC_LCTL, KC_SPC, MO(_LOWER),       MO(_RAISE), KC_ENT,  KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /* LOWER
@@ -163,7 +162,7 @@ static void print_status_narrow(void) {
             oled_write_ln_P(PSTR("Qwrt"), false);
             break;
         case _GAMER:
-            oled_write_ln_P(PSTR("Gamer"), false);
+            oled_write_P(PSTR("Gamer"), false);
             break;
         default:
             oled_write_P(PSTR("Undef"), false);
